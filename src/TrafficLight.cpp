ae1454dc0028b68ff0d6c37872c4bd9a08541bae
t-vince <vincent.tanghe@collaboration-factory.de>
2020-06-01 13:04:18 +0200

    Task FP.5

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -13,6 +13,16 @@ T MessageQueue<T>::receive()
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
+    
+    // perform queue modification under the lock
+    std::unique_lock<std::mutex> uLock(_mutex);
+    _cond.wait(uLock, [this] { return !_queue.empty(); }); // pass unique lock to condition variable
+
+    // remove last vector element from queue
+    T msg = std::move(_queue.back());
+    _queue.pop_back();
+
+    return msg;
 }
 
 template <typename T>
@@ -37,6 +47,16 @@ void TrafficLight::waitForGreen()
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
+
+    while (true) {
+        
+        auto currentPhase = _queue.receive();
+        if (currentPhase == green) {
+            return;
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
@@ -66,7 +86,7 @@ void TrafficLight::cycleThroughPhases()
     int waitDuration = getRandomWaitTime(4, 6);
     auto lastSwitchedTime = std::chrono::system_clock::now();
 
-    while(true) {
+    while (true) {
         auto currentLoopTime = std::chrono::system_clock::now();
         int SecondsSinceSwitch = std::chrono::duration_cast<std::chrono::seconds>
                                 (currentLoopTime - lastSwitchedTime)
